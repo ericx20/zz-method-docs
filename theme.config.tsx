7c09bba93d427d731a6a3a394fd62a8025a57d52
Eric Xu <ericxu2048@gmail.com>
2023-09-05 12:41:28 -0400

    Add branding

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,51 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import logo from "public/logo.svg";
 
 const config: DocsThemeConfig = {
-  logo: <span>ZZ Method</span>,
+  logo: (
+    <>
+      <Image src={logo} alt="ZZ Logo" width={40} />
+      <span>ZZ Method</span>
+    </>
+  ),
   project: {
     link: "https://github.com/ericx20/zz-method-docs",
   },
   // chat: {
   //   link: "https://discord.com",
   // },
-  docsRepositoryBase: "https://github.com/ericx20/zz-method-docs",
+  docsRepositoryBase: "https://github.com/ericx20/zz-method-docs/tree/main",
+  head() {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://zzmethod.com" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "ZZ Method"} />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description || "Guide to the ZZ Speedsolving Method"
+          }
+        />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </>
+    );
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    const isHomepage = asPath === "/";
+    const titleTemplate = isHomepage ? "ZZ Method" : "%s – ZZ Method";
+    return { titleTemplate };
+  },
   footer: {
     text: (
       <p>
