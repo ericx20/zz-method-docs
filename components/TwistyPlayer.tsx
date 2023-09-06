9a208e1ad175c30d07b1e89f14f579c257dc050f
Eric Xu <ericxu2048@gmail.com>
2023-09-05 20:49:08 -0400

    add TwistyPlayer 3d cube

---
+++ components/TwistyPlayer.tsx
@@ -0,0 +1,35 @@
+// taken from
+// https://github.com/cubing/react-cubing/blob/main/src/TwistyPlayer/index.tsx
+import * as React from "react";
+import { TwistyPlayer as TP, TwistyPlayerConfig } from "cubing/twisty";
+
+export interface TwistyPlayerExtendedConfig extends TwistyPlayerConfig {
+  className?: string;
+  onTwistyInit?: (twisty: TP) => void;
+}
+
+export default function TwistyPlayer({
+  className,
+  onTwistyInit,
+  ...props
+}: TwistyPlayerExtendedConfig) {
+  const [, setTwisty] = React.useState<TP>();
+  const spanRef = React.useRef<HTMLSpanElement | null>(null);
+
+  React.useEffect(() => {
+    const newTwisty = new TP(props);
+    if (className) {
+      newTwisty.className = className;
+    }
+    newTwisty.style.background = "#80808040";
+    newTwisty.style.borderRadius = "5px";
+    setTwisty(newTwisty);
+    spanRef.current?.appendChild(newTwisty);
+    if (onTwistyInit) onTwistyInit(newTwisty);
+    return () => {
+      spanRef.current?.removeChild(newTwisty);
+    };
+  }, [props.alg]);
+
+  return <span id="twisty-header" className={className} ref={spanRef} />;
+}
