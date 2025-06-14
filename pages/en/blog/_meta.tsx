404ff35711e7a8e6ddaacbadaf65483b672a8903
Eric Xu <50253028+ericx20@users.noreply.github.com>
2025-06-14 12:19:53 -0400

    Add solves for Yoruba ZZ WR ao5 (#51)
    * Add solves for Yoruba ZZ WR ao5
    
    * Nits

--- pages/en/blog/_meta.tsx
+++ pages/en/blog/_meta.tsx
@@ -7,6 +7,7 @@ export default {
       "pagination": false
     }
   },
+  "yoruba-6.93-zzwr": "6.93 ZZ World Record ao5 by Yoruba",
+  "zz-example-solve-library": "Introducing ZZ Example Solve Library!",
   "story": "The story of ZZMethod.com",
-  "zz-example-solve-library": "Introducing ZZ Example Solve Library!"
 }
